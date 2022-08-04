f685d8188004a77cd4d09119649b836c9ca0626c
Kristijan Pirkovic <93116779+kpirkovic@users.noreply.github.com>
2022-08-04 19:16:02 +0200

    Added lot of stuff

---
+++ Projects/project-animations.js
@@ -0,0 +1,106 @@
+const tl = gsap.timeline({})
+
+tl.to('.loader', {
+    opacity: 0,
+    delay: .35,
+    duration: 1,
+})
+tl.from('.hero-content *, .hero-wrapper', {
+    opacity: 0,
+    ease: "Power2.easeOut",
+    y: 50,
+    stagger: .15,
+    duration: .75,
+}, .55)
+
+gsap.from(".dprocess-content, .dprocess-blocks", {
+scrollTrigger: {
+    trigger: ".dprocess-section",
+    start: "-50%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+gsap.from(".persona-section #head, .persona", {
+scrollTrigger: {
+    trigger: ".persona-section",
+    start: "-55%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+
+gsap.from("#portfolio-head *, .wireframe", {
+scrollTrigger: {
+    trigger: ".portfolio-section",
+    start: "-55%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+
+gsap.from(".unique-features-section #head h2, .unique-features-section #head li", {
+scrollTrigger: {
+    trigger: ".unique-features-section",
+    start: "-60%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+
+gsap.from(".user-testing-content, .user-testing-section .user-blocks", {
+scrollTrigger: {
+    trigger: ".user-testing-section",
+    start: "-60%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+
+gsap.from(".contact-head *, .contact-form", {
+scrollTrigger: {
+    trigger: ".contact-section",
+    start: "-75%",
+},
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
+});
+
+gsap.from(".style-content, .style-colors, .font-content", {
+    scrollTrigger: {
+        trigger: ".style-section",
+        start: "-50%",
+    },
+        opacity: 0, 
+        y: 60,
+        stagger: .15,
+        ease: "Power2.easeOut",
+        duration: .75,
+});
+
+
+VanillaTilt.init(document.querySelectorAll(".persona"), {
+    max: 6,  
+    glare: true,
+    "max-glare": .05,
+    easing: "cubic-bezier(.17,.67,.83,.67)",
+    scale: .97
+});
--- animations.js
+++ animations.js
@@ -261,98 +261,14 @@ faqTl.from(".accordion", {
     delay: -.5,
 });
 
-const contactTl = gsap.timeline({
-    scrollTrigger: {
-        start: '-95%',
-        trigger: contactSection,
-    }
-})
-contactTl.from(".contact-head h2, .contact-head p", {
-    opacity: 0,
-    ease: "Power2.easeOut",
-    y: 50,
-    stagger: .15,
-    duration: .7
-});
-contactTl.from(".contact-form", {
-    opacity: 0,
-    ease: "Power2.easeOut",
-    y: 50,
-    stagger: .15,
-    duration: .7,
-    delay: -.5,
-});
-
-//Hamburger Menu Open
-const navList = document.querySelector('.nav-list');
-const mql = window.matchMedia("(max-width: 1360px)");
-let isOpen = false;
-let isMobile = false;
-
-const navTl = gsap.timeline({});
-
-navTl.fromTo(navList, {
-    autoAlpha: 0, 
-    display:'none'
+gsap.from(".contact-head *, .contact-form", {
+scrollTrigger: {
+    trigger: contactSection,
+    start: "-95%",
 },
-{
-    autoAlpha: 1, 
-    display:'flex'
-})
-
-navTl.from('.nav-list a', { 
-    opacity: 0,
-    stagger: .05,
-    delay: -.5,
-    ease: "Power3.easeOut",
-    y: -50,
-    zindex: -1
-})    
-
-if(mql.matches) {
-    navTl.progress(0).reversed(true);
-    isOpen = false;
-    isMobile = true;
-}
-
-const burgerTl = gsap.timeline({ paused: true, repeatDelay: 0});
-
-burgerTl.to('#hamburger-btn span:nth-child(1)',{ width: 0, duration: .3 }, 0)
-    .to('#hamburger-btn span:nth-child(2)',{ rotation: -45}, 0)
-    .to('#hamburger-btn span:nth-child(3)',{ width: 0, duration: .3}, 0)
-
-burgerTl.timeScale(1.3);
-
-document.addEventListener('click', (e)=> {
-    if(e.target.id == 'hamburger-btn' && isMobile){
-        if(isOpen){
-            navTl.timeScale(1.8);
-            navTl.reverse();
-            isOpen = false;
-
-            burgerTl.reverse();
-        } else {
-            navTl.timeScale(1);
-            navTl.play(); 
-            isOpen = true;
-
-            burgerTl.play();
-        }
-    } else if (isOpen && isMobile) {
-        navTl.timeScale(1.8);
-        navTl.reverse();
-        burgerTl.reverse();
-    }
-})
-
-window.addEventListener("resize", function (e) {
-    if(e.target.innerWidth >= 1360) {
-        navTl.progress(1).play();
-        isOpen = true;
-        isMobile = false;
-    } else if (e.target.innerWidth <= 1360) {
-        navTl.progress(0).reversed(true);
-        isOpen = false;
-        isMobile = true;
-    }
+    opacity: 0, 
+    y: 60,
+    stagger: .15,
+    ease: "Power2.easeOut",
+    duration: .75,
 });
