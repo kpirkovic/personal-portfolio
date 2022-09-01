872f35e5a275a46af45ada77baf6e2cd21dc91e9
Kristijan Pirkovic <93116779+kpirkovic@users.noreply.github.com>
2022-09-01 15:25:05 +0200

    Update project-animations.js

--- Projects/project-animations.js
+++ Projects/project-animations.js
@@ -5,18 +5,19 @@ tl.to('.loader', {
     delay: .35,
     duration: 1,
 })
-tl.from('.hero-content *, .hero-wrapper', {
+tl.from('.browser', {
+    visible: false,
     opacity: 0,
     ease: "Power2.easeOut",
-    y: 50,
+    y: 70,
     stagger: .1,
-    duration: .75,
-}, .55)
+    duration: 1,
+}, 1)
 
-gsap.from(".dprocess-content, .dprocess-blocks", {
+gsap.from(".desc-content, .desc-buttons *", {
 scrollTrigger: {
-    trigger: ".dprocess-section",
-    start: "-60%",
+    trigger: ".cs-description",
+    start: "-50%",
 },
     opacity: 0, 
     y: 60,
@@ -40,30 +41,6 @@ gsap.from("#portfolio-head *, .wireframe", {
 scrollTrigger: {
     trigger: ".portfolio-section",
     start: "-65%",
-},
-    opacity: 0, 
-    y: 60,
-    stagger: .15,
-    ease: "Power2.easeOut",
-    duration: .75,
-});
-
-gsap.from(".unique-features-section #head h2, .unique-features-section #head li", {
-scrollTrigger: {
-    trigger: ".unique-features-section",
-    start: "-70%",
-},
-    opacity: 0, 
-    y: 60,
-    stagger: .15,
-    ease: "Power2.easeOut",
-    duration: .75,
-});
-
-gsap.from(".user-testing-content, .user-testing-section .user-blocks", {
-scrollTrigger: {
-    trigger: ".user-testing-section",
-    start: "-70%",
 },
     opacity: 0, 
     y: 60,
